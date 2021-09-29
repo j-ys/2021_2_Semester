bca6e76ff122f58e23a9a1bdc866f3d62379b765
Young joon Lee <87681631+UserYoungJoon@users.noreply.github.com>
2021-09-29 17:22:44 +0900

    commit test

--- MainApplication/app/src/main/java/com/example/mainapplication/MainActivity.java
+++ MainApplication/app/src/main/java/com/example/mainapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mainapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button1);
+
     }
+}
+
+public void inClikedButton(View v) {
+    Toast.makeText(context: this,)
 }
